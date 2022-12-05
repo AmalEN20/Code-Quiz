b93259d5fd45a84ef8207fb77e71746f5d37e0a8
AmalStudy <amalengstudy@gmail.com>
2022-12-04 17:50:36 -0800

    game files

---
+++ assets/js/game.js
@@ -0,0 +1,5 @@
+const question = document.querySelector('#question');
+const choices = Array.from(document.querySelectorAll('.choice-text'));
+const progreesText = document.querySelector('#progreesText');
+const scoreText = document.querySelector('#score');
+const progreesBarFull = document.querySelector('#progreesBarFull');
