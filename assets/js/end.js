9556102c6f292980bafa9e73a071bb2755ae0374
AmalStudy <amalengstudy@gmail.com>
2022-12-05 18:04:18 -0800

    fix folders

--- assets/js/end.js
+++ assets/js/end.js
@@ -1,9 +1,9 @@
-const username = document.querySelector(`#username`)
-const saveScoreBtn = document.querySelector(`#saveScoreBtn`)
-const finalScore = document.querySelector(`#finalScore`)
-const mostRecentScore = localStorage.getItem(`mostRecentScore`)
+const username = document.querySelector('#username')
+const saveScoreBtn = document.querySelector('#saveScoreBtn')
+const finalScore = document.querySelector('#finalScore')
+const mostRecentScore = localStorage.getItem('mostRecentScore')
 
-const highScores = JSON.parse(localStorage.getItem('highScires')) || []
+const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
 const MAX_HIGH_SCORES = 5
 
@@ -15,17 +15,22 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = e => {
     e.preventDefault()
-    
-    const score ={
+
+    const score = {
         score: mostRecentScore,
-        name:username.value
+        name: username.value
     }
+
     highScores.push(score)
+
     highScores.sort((a,b) => {
         return b.score - a.score
-    } )
+    })
+
     highScores.splice(5)
 
     localStorage.setItem('highScores', JSON.stringify(highScores))
     window.location.assign('/')
+
+    
 }
